ec15d46bb024b716df738ba9dcf01d4bd34badb1
Hector DelaRosa <hdelaros@gmail.com>
2022-07-10 14:57:02 -0400

    delete route

--- src/server/routes/employee.router.js
+++ src/server/routes/employee.router.js
@@ -18,8 +18,15 @@ employeesRouter.get('/:id', async (req, res) => {
     res.status(400).send(err);
   }
 });
-
-employeesRouter.post('/:id', async (req, res) => {
+employeesRouter.post('/', async (req, res) => {
+  try {
+    const newEmployee = await Employee.create(req.body);
+    return res.status(201).send(newEmployee);
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+employeesRouter.patch('/:id', async (req, res) => {
   try {
     const foundEmployee = await Employee.findOne({
       where: { id: req.params.id },
@@ -31,5 +38,27 @@ employeesRouter.post('/:id', async (req, res) => {
   }
 });
 
+employeesRouter.delete('/:id', async (req, res) => {
+    try {
+      const employee = await Employee.findOne({
+        where: {
+          id: req.params.id,
+        },
+      });
+  
+      if (employee === null) {
+        res.status(404).send('Employee could not be found');
+      } else {
+        await Employee.destroy({
+          where: {
+            id: req.params.id,
+          },
+        });
+        return res.status(204).send('Employee deleted');
+      }
+    } catch (err) {
+      res.status(400).send(err);
+    }
+  });
 
 module.exports = employeesRouter;
