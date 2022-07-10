4d4aad141ee0463ab4471085351679fe7210ce33
Hector DelaRosa <hdelaros@gmail.com>
2022-07-10 15:01:39 -0400

    static index.html

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-//mounting routes on their own router to oganize code and help with debugging
+//mounting routes on their own router to organize code and help with debugging
 router.use('/companies', require('./companies.router'));
 router.use('/employees', require('./employees.router'));
 
