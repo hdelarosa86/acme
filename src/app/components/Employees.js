bdf76e8d9bf5142fa9177344830094be00f0d731
Hector DelaRosa <hdelaros@gmail.com>
2022-07-10 15:30:42 -0400

    Fixed typo preventing Employees from rendering

---
+++ src/app/components/Employees.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+const Employees = () => {
+  const [employees, setEmployees] = useState([]);
+
+  const getEmployees = async () => {
+    const { data } = await axios.get('/api/employees');
+    setEmployees(data);
+  };
+
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {employees.map((employee) => (
+          <li key={employee.id}>
+            <Link to={`/employees/${employee.id}`}>
+              <EmployeeCard
+                id={employee.id}
+                firstName={employee.firstName}
+                lastName={employee.lastName}
+                email={employee.email}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Employees;
